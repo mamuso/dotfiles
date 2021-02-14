ba3a705b85db55a214782a21bfc44e929515233e
Manuel Muñoz Solera <Manuel Muoz Solera>
2021-02-13 22:18:31 -0800

    zsh initial files

---
+++ .init/zsh.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+MEDIUMORCHID='\033[38;5;207m'
+GREY='\033[38;5;240m'
+NC='\033[0m' # No Color
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
