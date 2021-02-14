edb30bbf30adda92c346e35ad848e6adb2c56832
Manuel Muñoz Solera <mamuso@mamuso.net>
2021-02-14 00:35:41 -0800

    zsh path

--- .init/zsh.sh
+++ .init/zsh.sh
@@ -5,4 +5,5 @@ GREY='\033[38;5;240m'
 NC='\033[0m' # No Color
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fpath+=('/Users/mamuso/.npm-global/lib/node_modules/pure-prompt/functions')
 chsh -s $(which zsh)
